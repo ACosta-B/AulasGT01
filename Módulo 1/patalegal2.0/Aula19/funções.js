85a19a463e71d29c55b7de652d7e788ee1703efb
A. Costa Brunes <brunes.alice62@gmail.com>
2025-04-28 11:57:07 -0300

    Add files via upload

---
+++ Módulo 1/patalegal2.0/Aula19/funções.js
@@ -0,0 +1,13 @@
+function depositar_valor(deposito,saldo) {
+    if (saldo += deposito) {
+      return saldo;
+      alert(`deposito realizado com sucesso!`);
+    }
+}
+function sacar_valor(saque,saldo) {
+    if (saldo >= saque) {
+        saldo -= saque
+        alert(`Saldo insuficiente`);
+    }
+    
+}
